60c9f57a7243fdfec3e14d4298ee479cab40d933
Tobias Haugeland <tobias.haugeland@gmail.com>
2024-11-30 17:04:46 +0100

    wip: not working...

--- examples/grandma_connect/psn_server.py
+++ examples/grandma_connect/psn_server.py
@@ -2,20 +2,17 @@ import psn
 import time
 import socket
 import json
-import asyncio
-import websockets
+from aiohttp import web
+import logging
 
 PSN_DEFAULT_UDP_PORT = 56565
 PSN_DEFAULT_UDP_MCAST_ADDRESS = "236.10.10.10"
 
-WS_PORT = 8000
-WS_IP = "localhost"
-
-# Global position variable
-position = psn.Float3(0, 0, 0)
+PORT = 8000
+IP = "0.0.0.0"
+SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-# Helper functions
 def get_time_ms():
     return int(time.time() * 1000)
 
@@ -28,56 +25,50 @@ def get_elapsed_time_ms():
 
 
 def pic_to_scene_coords(x, y):
-    return x / 100, y / 100
+    return x / 200, y / 200
 
 
-async def handle_websocket(websocket):
-    global position
-    async for message in websocket:
-        data = json.loads(message)
-        x, y = pic_to_scene_coords(data["x"], data["y"])
-        position = psn.Float3(x, y, 0)
+def send_position(x: float, y: float, id: int = 0):
+    global SOCK
 
-
-async def run_psn_server():
-    print("Starting PSN server")
-    # Setup a UDP socket to send tracker packets on
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Encoder / Decoder
     encoder = psn.Encoder("Server 1")
-    trackers = {}
-    trackers[0] = psn.Tracker(0, "Tracker 0")
-    trackers[0].set_pos(psn.Float3(0, 0, 0))
+    tracker = psn.Tracker(id, f"Tracker {id}")
+    tracker.set_pos(psn.Float3(x, y, 0))
+    packet = encoder.encode_data(tracker, get_elapsed_time_ms())
+    SOCK.sendto(packet, (PSN_DEFAULT_UDP_MCAST_ADDRESS, PSN_DEFAULT_UDP_PORT))
 
-    last_sent_info = get_elapsed_time_ms()
-    last_sent_data = get_elapsed_time_ms()
 
-    packets = []
+async def handle_websocket(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    for msg in ws:
+        if msg.type == web.WSMsgType.TEXT:
+            logging.debug("Received message: %s" % msg.data)
 
-    async with websockets.serve(handle_websocket, WS_IP, WS_PORT):
-        global position
-        while True:
-            time_stamp = get_elapsed_time_ms()
+            # data = json.loads(msg.data)
+            # x, y = pic_to_scene_coords(data["x"], data["y"])
+            # send_position(x, y)
+            # ws.send_str(json.dumps({"x": x, "y": y}))
+        elif msg.type == web.WSMsgType.ERROR:
+            logging.error("ws connection closed with exception %s" % ws.exception())
+            # print("ws connection closed with exception %s" % ws.exception())
 
-            # Approx 30 fps
-            if time_stamp - last_sent_data >= 33:
-                trackers[0].set_pos(position)
-                packets.extend(encoder.encode_data(trackers, time_stamp))
-                last_sent_data = time_stamp
-                print(position.x, position.y, end="\r")
+    return ws
 
-            if time_stamp - last_sent_info >= 1000:
-                packets.extend(encoder.encode_info(trackers, time_stamp))
-                last_sent_info = time_stamp
 
-            for p in packets:
-                sock.sendto(p, (PSN_DEFAULT_UDP_MCAST_ADDRESS, PSN_DEFAULT_UDP_PORT))
 
-            packets.clear()
+async def handle_root(request):
+    return web.FileResponse("./static/index.html")
 
-            await asyncio.sleep(0.001)
+
+def create_app():
+    app = web.Application()
+    app.router.add_get("/", handle_root)
+    app.router.add_static("/", "./static")
+    app.router.add_get("/ws", handle_websocket)
+    return app
 
 
 if __name__ == "__main__":
-    asyncio.run(run_psn_server())
+    logging.basicConfig(level=logging.DEBUG)
+    web.run_app(create_app(), host=IP, port=PORT)
